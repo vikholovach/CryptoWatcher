8d919ec0d1789789c70e28798b256494b7110e6c
Viktor Holovach <viktorgolovach@Viktors-MacBook-Pro.local>
2023-09-10 15:04:20 +0200

    initial commit

---
+++ CryptoWathcer/CryptoWathcer/CryptoWathcerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoWathcerApp.swift
+//  CryptoWathcer
+//
+//  Created by Viktor Golovach on 10.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoWathcerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
