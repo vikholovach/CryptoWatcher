8254cbd6b95964e9a6c5b05f8cf28d168e35f264
Viktor Holovach <viktorgolovach@Viktors-MacBook-Pro.local>
2023-09-21 22:23:11 +0200

    fix description view appearence

--- CryptoWathcer/CryptoWathcer/View/WebView/WebViewContainer.swift
+++ CryptoWathcer/CryptoWathcer/View/WebView/WebViewContainer.swift
@@ -12,19 +12,17 @@ struct WebViewContainer: View {
     @State var coin: CoinData
     
     var body: some View {
-        NavigationView {
-            WebView(coin: $coin)
-                .navigationBarTitle(coin.name)
-                .padding(.top, 8)
-                .padding(.trailing, 24)
-                .padding(.leading, 24)
-                .padding(.bottom, 0)
-                .background(ColorfulView())
-        }
-        
-        
+        WebView(coin: $coin)
+            .navigationBarTitle(coin.name)
+            .padding(.top, 8)
+            .padding(.trailing, 24)
+            .padding(.leading, 24)
+            .padding(.bottom, 0)
+            .background(ColorfulView())
     }
+    
 }
+
 
 struct WebViewContainer_Previews: PreviewProvider {
     
